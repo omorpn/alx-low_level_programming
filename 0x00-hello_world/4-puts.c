20cdd671f6eab2fb537babb8c0e04c7d891fea92
Tunde Daniel <omo.hope.blog@gmail.com>
2022-12-08 16:10:02 -0800

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main - this is the main path of this code that perform the function 
+ *main - this is the main path of this code that perform the function
  *
  *
- * return - always return 0 at all time
+ *return - always return 0 at all time
  */
 int main(void)
 	{
 		char west[55];
-		
 		strcpy(west, "\"Programming is like building a multilingual puzzle");
 		puts(west);
 		return (0);
