1c9be7c465941eb676c9a898627b67ee39563faf
Tunde Daniel <omo.hope.blog@gmail.com>
2022-12-08 16:04:55 -0800

    Write a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - this is the main path of this code that perform the function 
+ *
+ *
+ * return - always return 0 at all time
+ */
+int main(void)
+	{
+		char west[55];
+		
+		strcpy(west, "\"Programming is like building a multilingual puzzle");
+		puts(west);
+		return (0);
+	}
