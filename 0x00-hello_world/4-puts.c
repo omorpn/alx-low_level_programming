4ed6e849405cb47f731e12604175cb98b45f62ad
Tunde Daniel <omo.hope.blog@gmail.com>
2022-12-09 05:16:09 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+
 /**
  * main - this is my first c program
  *
  * Return: return 0 everytime
  */
+
 int main(void)
 {
-	char displayed{52};
-
-	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
-	puts(displayed);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
