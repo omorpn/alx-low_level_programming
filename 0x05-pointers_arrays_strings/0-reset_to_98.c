d58a8ad49caebbe4fe291d3570a1cb24f8ca9609
Tunde Daniel <omo.hope.blog@gmail.com>
2022-12-20 15:37:16 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * reset_to_98 - this helps to return the value to 98
+ * @n : helps to do the computation
+ */
+
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is file for arrys aND POINTERS
