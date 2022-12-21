9783b3740fbc894b743ff9a5917131b0b6a8e88e
Tunde Daniel <omo.hope.blog@gmail.com>
2022-12-20 19:05:45 -0800

    arry pointers

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,30 +1,43 @@
-#include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
 /**
- * main - generates random valid passwords
- * return: 0 (on success).
+ * main - random password generator for 101-crackme
  *
+ * Return: always 0
  */
-
 int main(void)
 {
-
-	int pass, sum;
-
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
 
 	srand(time(NULL));
-
-	sum = 0;
-	while (sum <= 2645)
+	while (s != 2772)
 	{
-		pass = (rand()  %  128);
+		i = k = s = 0;
 
-		sum += pass;
-		printf("%c", pass);
+		while ((2772 - 122) > s)
+		{
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
+		}
 	}
-	printf("%c", 2772 - sum);
-
+	p[i] = '\0';
+	printf("%s", p);
 	return (0);
 }
