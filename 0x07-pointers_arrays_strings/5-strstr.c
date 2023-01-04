2d2427673adc13e40b3052195682ee0766af2b8f
Tunde Daniel <omo.hope.blog@gmail.com>
2023-01-03 21:19:13 -0800

    this is more pointers

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,28 +2,39 @@
 #define NULL 0
 
 /**
-*   _strpbrk - return pointer to byte in s that matches a byte in accept
-*    @s: string to search
-*     @accept: target matches
-*      Return: pointer to index of string at first occurence
+*   _strstr - locate and return pointer to first occurence of substring
+*    @haystack: string to search
+*     @needle: target substring to search for
+*  Return: pointer to index of string at first occurence of whole substring
 */
 
-char *_strpbrk(char *s, char *accept)
+char *_strstr(char *haystack, char *needle)
 {
-int x = 0, y;
-while (s[x] != '\0')
-{
-for (y = 0; accept[y] != '\0'; y++)
-{
-if (s[x] == accept[y])
-{
-s = &s[x];
-return (s);
-}
+int i = 0, j, x;
 
-}
-x++;
-}
+if (needle[0] == '\0')
+return (haystack);
 
-return (NULL);
+while (haystack[i] != '\0') /* iterate through haystack */
+{
+/* if a byte matches first char of needle */
+/* interate through needle until match ends */
+if (haystack[i] == needle[0])
+{
+x = i, j = 0;
+while (needle[j] != '\0')
+{
+if (haystack[x] == needle[j])
+x++, j++;
+else
+			break;
+} /* if matched throughout, return haystack */
+if (needle[j] == '\0')
+{
+return (haystack + i);
+}
+}
+i++;
+}
+return (NULL); /* No match */
 }
